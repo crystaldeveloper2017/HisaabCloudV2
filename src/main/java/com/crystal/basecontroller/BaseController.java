44f5949bd61ad95976dcdce8d190edaff660f242
Crystal Developers <crystaldevelopers2017@gmail.com>
2023-06-06 16:24:53 +0530

    fixed basecontroller issue

--- src/main/java/com/crystal/basecontroller/BaseController.java
+++ src/main/java/com/crystal/basecontroller/BaseController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.crystal.Frameworkpackage.ControllerServiceImpl;
 
-@WebServlet("/BaseController")
+@WebServlet(name = "BaseController" , urlPatterns = {""})
 public class BaseController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
